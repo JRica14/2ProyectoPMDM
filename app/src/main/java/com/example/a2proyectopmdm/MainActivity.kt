3645783ccabb8c7e310eff4dd07f2a688d251809
Jrica14 <Jorric437@alu.centroafuera.es>
2022-11-17 14:02:08 +0100

    Segundo Commit

--- app/src/main/java/com/example/a2proyectopmdm/MainActivity.kt
+++ app/src/main/java/com/example/a2proyectopmdm/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a2proyectopmdm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     private var onCreate=0
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     private var onStop=0
     private var onDestroy=0
     private var onRestart=0
+    var t: String? = null
+    var texto: EditText? = null
+    var texto1: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,12 +47,31 @@ class MainActivity : AppCompatActivity() {
         println("onRestart $onRestart")
     }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
+    override fun onSaveInstanceState(guardaEstado: Bundle) {
+        super.onSaveInstanceState(guardaEstado)
+        //guardamos en la variable t el texto del campo EditText
+        texto = findViewById (R.id.editTextTextPersonName);
+        t = texto!!.text.toString()
+        //lo "guardamos" en el Bundle
+        guardaEstado.putString("text", t)
+        //guardamos en la variable t el texto del campo EditText
+        texto1 = findViewById (R.id.editTextTextPersonName2);
+        t = texto1!!.text.toString()
+        //lo "guardamos" en el Bundle
+        guardaEstado.putString("text1", t)
+
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
+    override fun onRestoreInstanceState(recuperaEstado: Bundle) {
+        super.onRestoreInstanceState(recuperaEstado)
+        //recuperamos el String del Bundle
+        t = recuperaEstado.getString("text")
+        //Seteamos el valor del EditText con el valor de nuestra cadena
+        texto!!.setText(t)
+        //recuperamos el String del Bundle
+        t = recuperaEstado.getString("text1")
+        //Seteamos el valor del EditText con el valor de nuestra cadena
+        texto1!!.setText(t)
     }
 
 }
